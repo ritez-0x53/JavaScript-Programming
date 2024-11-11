6b3c12ae24759c08af231912af3da3691b229b51
ritez-0x5253 <riteswarsrgy@gmail.com>
2024-11-11 21:13:27 +0530

    coded some string methods

--- 2.Operators&Strings/strings.js
+++ 2.Operators&Strings/strings.js
@@ -7,7 +7,7 @@ let backtick = `Hello`
 const greet = `${singleQuote}, Riteswar`
 console.log(greet);
 
-const text = "Hello, JavaScript"
+let text = "Hello, JavaScript"
 
 // returns number of characters in a string
 console.log(text.length);
@@ -41,6 +41,33 @@ console.log(text.substring(7 , text.length));
 
 // substr(start, length): Extracts a substring starting from start with the specified length.
 console.log(text.substr(7,4));
+
+//*** Modifying Strings */ 
+text = "This is my JavaScript Coding , JavaScript is best"
+
+// replace(searchValue, newValue): Replaces the first occurrence of searchValue with newValue.
+const replacedText = text.replace("JavaScript" , "Solidity");
+console.log(replacedText)
+
+// replaceAll(searchValue, newValue): Replaces all occurrences of searchValue with newValue (introduced in ES2021).
+const replaceAllText = text.replaceAll("JavaScript" , "Python");
+console.log(replaceAllText);
+
+// trim(): Removes whitespace from both ends of the string.
+let newText = "   Java  "
+console.log(newText.trim());
+
+//*** Splitting and Joining Strings */
+
+// split(separator): Splits a string into an array of substrings based on a specified separator.
+const text2 = "Demo Text";
+console.log(text2.split(""));
+console.log(text2.split(" "));
+
+// concat(...strings): Combines multiple strings into one.
+console.log(newText.trim().concat(" ",text2));
+
+
 
 
 
